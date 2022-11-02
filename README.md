2ac6d2a706d6b16b69cbe25f64e5e5149f2fef88
twd10 <tzuoyeah@gmail.com>
2022-11-03 01:14:12 +0800

    新增Comments

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### 資料夾結構參考
 https://medium.com/js-dojo/modern-frontend-architecture-101-f9c88c20ea20
+
+### TypeSceipt Comments 參考
+https://tsdoc.org/
--- src/feature/DateEventView.tsx
+++ src/feature/DateEventView.tsx
@@ -5,15 +5,38 @@ import Typography from '@mui/material/Typography'
 
 import DateEventViewList from 'subfrature/DateEventView_List'
 import NoteEvent from "api/NoteEvent"
+
+// #region Type
 type Props = {
-  noteEventList?: NoteEvent[];
-};
+  noteEventList: NoteEvent[];
+}
+
 type Group = {
   month: number;
   noteEventList: NoteEvent[];
-};
+}
+// #endregion
 
-function reduceNoteEventList(noteEventList:NoteEvent[]){
+/**
+ * DateEven的清單
+ */
+export default function DateEventView({noteEventList}:Props) {
+  const GroupList = reduceNoteEventList(noteEventList)
+  sortMonthGroup(GroupList)  
+  return (
+    <Paper elevation={0} variant="outlined" sx={{p:'8px'}}>
+      <Typography m={0} variant="h6" gutterBottom sx={{fontWeight:'bold'}}>檢視</Typography>
+      {GroupList.map((group,key)=>
+        <DateEventViewList month={group.month} noteEventList={group.noteEventList} key={key}/>
+      )}
+    </Paper>
+  );
+}
+
+/**
+ * 將無序的NoteEvent陣列轉成以月份為群組的Group陣列
+ */
+ function reduceNoteEventList(noteEventList:NoteEvent[]):Group[]{
   return noteEventList.reduce((groupList,dateEvent)=>{
     const month = dateEvent.date.getMonth()
     const groupIndex = groupList.findIndex(element => element.month ===month)??0
@@ -25,19 +48,9 @@ function reduceNoteEventList(noteEventList:NoteEvent[]){
   },new Array<Group>())
 }
 
+/**
+ * 將Group陣列中的NoteEvent以日期做排序
+ */
 function sortMonthGroup(GroupList:Group[]){
   GroupList.forEach(element => element.noteEventList.sort((a,b) => a.date.getDate() - b.date.getDate() ));
-}
-
-export default function DateEventView(props:Props) {
-  const GroupList = props.noteEventList?reduceNoteEventList(props.noteEventList):[]
-  sortMonthGroup(GroupList)  
-  return (
-    <Paper elevation={0} variant="outlined" sx={{p:'8px'}}>
-      <Typography m={0} variant="h6" gutterBottom sx={{fontWeight:'bold'}}>檢視</Typography>
-      {GroupList.map((group,key)=>
-        <DateEventViewList month={group.month} noteEventList={group.noteEventList} key={key}/>
-      )}
-    </Paper>
-  );
 }
